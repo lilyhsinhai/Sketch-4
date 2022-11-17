8096701719684a504626238dea870f8a7e40b47d
lilyhsinhai <lha5017@nyu.edu>
2022-11-17 13:28:07 -0500

    tiles appear

---
+++ quilt.js
@@ -0,0 +1,29 @@
+const quilts = [];
+
+function setup() {
+  createCanvas(800, 800);
+  quilts.push(loadImage('quilts/redBorder.jpg'));
+  quilts.push(loadImage('quilts/pinkMandala.jpg'));
+  quilts.push(loadImage('quilts/parasol.jpg'));
+  quilts.push(loadImage('quilts/lilypads.jpg'));
+
+}
+
+function draw() {
+  tileQuilt();
+}
+
+function tileQuilt() {
+  var x = 0;
+  var y = 0;
+  var gridSize = sqrt(quilts.length);
+  var step = width / gridSize;
+  for (const quilt of quilts) {
+    image(quilt, x, y, x + step, y + step);
+    x += step;
+    if (x >= width) {
+      x = 0;
+      y += step;
+    }
+  }  
+}
